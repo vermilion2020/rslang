3e22dfda933fa9391137b71a2e266e946c8bdb0b
Halina Kulakova <kalisakulakova@gmail.com>
2022-08-21 00:07:19 +0200

    fix: footer and header edits

--- src/components/view/main/MainTemplate.ts
+++ src/components/view/main/MainTemplate.ts
@@ -1,7 +1,8 @@
 const mainTemplate: HTMLTemplateElement = document.createElement('template');
 mainTemplate.innerHTML = `
   <div class="main-page">
-    <h2>Main Page</h2>
-    <h3>Some Main Content</h3>
+    <h3>Изучай английский
+    с помощью игр</h3>
+    <h3>"Neque porro quisquam est qui doRRRRR lorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h3>
   </div>`;
 export default mainTemplate;
