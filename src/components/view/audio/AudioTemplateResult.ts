1520943550dcc3df570be396e9d4d6371a715dc4
googray <shat.rom07@gmail.com>
2022-08-23 20:58:15 +0200

    feat: some new layout change scss in popup resu

--- src/components/view/audio/AudioTemplateResult.ts
+++ src/components/view/audio/AudioTemplateResult.ts
@@ -21,7 +21,7 @@ audioTemplateResult.innerHTML = `
 export const audioTemplateWords: HTMLTemplateElement = document.createElement('template');
 audioTemplateWords.innerHTML = `
 <form id="page-words" class="popupAudio">
-<div class="wrapper">
+    <div class="wrapper">
         <div class="head-result">
           <h3 id="result-value" class="result-value">Результат</h3>
           <h3 id="result-words" class="result-words">Посмотреть слова</h3>
@@ -30,7 +30,7 @@ audioTemplateWords.innerHTML = `
           <ul class="result-correct">
           <li class="correct-item"><div class="on-speak"><img class='img-voice' src="../../../assets/images/png/up_volume.png" alt="img voice"></div><span class="eng-word"> - {englishWord}</span><span class="rus-word"> - {russianWord}</span></li>
           </ul>
-          <hr>
+          <hr class="hr-line">
           <ul class="result-incorrect">
             <li class="incorrect-item"></li>
           </ul>
@@ -39,6 +39,6 @@ audioTemplateWords.innerHTML = `
           <button class="result-repeat">Повторить</button>
           <button class="result-exit">Выйти</button>
         </div>
-      </div>
+    </div>
 </form>
 `;
