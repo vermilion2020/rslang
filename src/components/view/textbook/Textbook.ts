dfed45386e1e887de7ab22b4b330d8f43b92de69
vermilion2020 <75521323+vermilion2020@users.noreply.github.com>
2022-08-23 16:17:33 +0300

    fix: paging for textbook (#5)
    fix: paging for textbook

--- src/components/view/textbook/Textbook.ts
+++ src/components/view/textbook/Textbook.ts
@@ -21,6 +21,40 @@ class Textbook implements Page {
     return this.state;
   }
 
+  handleButtonsState(first: boolean, last: boolean) {
+    const prev = <HTMLElement>document.querySelector('.paging__prev');
+    const next = <HTMLElement>document.querySelector('.paging__next');
+    first ? prev.classList.add('disabled') : prev.classList.remove('disabled');
+    last ? next.classList.add('disabled') : next.classList.remove('disabled');
+  }
+
+  async handlePagingClick(e: Event, countPages: number) {
+    const target = e.target as HTMLElement;
+    let last = false;
+    let first = false;
+    if (target.dataset.number) {
+      this.state.textbook.page = +target.dataset.number;
+    } else if (target.classList.contains('paging__prev')) {
+      if (this.state.textbook.page <= 1) {
+        this.state.textbook.page = 1;
+        first = true;
+      } else {
+        --this.state.textbook.page;
+      }
+    } else if (target.classList.contains('paging__next')) {
+      if (this.state.textbook.page >= countPages) {
+        this.state.textbook.page = countPages;
+        last = true;
+      } else  {
+        ++this.state.textbook.page;
+      }
+    }
+    this.handleButtonsState(first, last);
+    if(!first && !last) {
+      await this.changeCurrentPage(1, this.state.textbook.page);
+    }
+  }
+
   async changeCurrentPage(unit: number, page: number) {
     // TODO add units paging here also and save together
     window.location.hash = `/${this.state.page}/unit${unit}/${page}`;
@@ -32,18 +66,11 @@ class Textbook implements Page {
 
   paging() {
     // TODO contPages should be received from BE and calculated
-    const contPages = 5;
-    const pagingNode = <HTMLElement>pagingTemplate(contPages, this.state.textbook.page).content.cloneNode(true);
+    const countPages = 5;
+    const pagingNode = <HTMLElement>pagingTemplate(countPages, this.state.textbook.page).content.cloneNode(true);
     const paging = <HTMLElement>pagingNode.querySelector('.paging');
     paging.addEventListener('click', async (e) => {
-      const target = e.target as HTMLElement;
-      if (target.dataset.number) {
-        await this.changeCurrentPage(1, +target.dataset.number);
-      } else if (target.classList.contains('paging__prev')) {
-        await this.changeCurrentPage(1, this.state.textbook.page - 1);
-      } else if (target.classList.contains('paging__next')) {
-        await this.changeCurrentPage(1, this.state.textbook.page + 1);
-      }
+      this.handlePagingClick(e, countPages);
     });
     return pagingNode;
   }
