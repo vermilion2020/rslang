113831b14734871b0464cc4ff50be866e3b53642
Halina Kulakova <kalisakulakova@gmail.com>
2022-08-30 17:09:54 +0200

    feat: add main layout

--- src/components/view/dictionary/Dictionary.ts
+++ src/components/view/dictionary/Dictionary.ts
@@ -1,6 +1,9 @@
-import dictionaryTemplate from './DictionaryTemplate';
 import './Dictionary.scss';
+import '../textbook/GenerForWords.scss';
 import { Page, PagesState } from '../../model/types/page';
+import { pagingTemplate, unitTemplate, sectionWords, titleTemplate} from './DictionaryTemplate';
+import { loadWords, loadWordsHard } from '../../controller/helpers/word-helper';
+import { handleRoute } from '../../controller/router';
 
 class Dictionary implements Page {
   state: PagesState;
@@ -11,12 +14,84 @@ class Dictionary implements Page {
 
   async render() {
     this.state.page = 'dictionary';
-    const notFoundNode = <HTMLElement>dictionaryTemplate.content.cloneNode(true);
     const container = document.querySelector('#main-container') as HTMLDivElement;
+    const sectionDictionary = await this.createSectionDiction();
+    // const sectionPlay = this.createSectionPlay();
     container.innerHTML = '';
-    container.append(notFoundNode);
+    container.append(sectionDictionary);
     return this.state;
   }
+
+  async createSectionDiction() {
+    const { section, wrapper } = <Record<string, HTMLElement>>sectionWords(this.state.dictionary.unit);
+    const titleNode = <HTMLElement>titleTemplate('Словарь').content.cloneNode(true);
+    let words = await loadWords(this.state.dictionary.unit, this.state.dictionary.page, this.state.loggedIn);
+    if (this.state.dictionary.unit === 7) {
+      words = await loadWordsHard(this.state);
+    }
+    // const textbookNode = <HTMLElement>dictionaryTemplate(words).content.cloneNode(true);
+    const pagingNode = this.paging();
+    const unitNode = this.units();
+  
+    wrapper.innerHTML = '';
+    wrapper.append(titleNode);
+    wrapper.append(unitNode);
+    // wrapper.append(textbookNode);
+    wrapper.append(pagingNode);
+    return section;
+  }
+
+  paging() {
+    const pagingNode = <HTMLElement>pagingTemplate(this.state.dictionary.unit, this.state.dictionary.page)
+      .content.cloneNode(true);
+    const paging = <HTMLElement>pagingNode.querySelector('.paging');
+    paging.addEventListener('click', async (e) => {
+      this.handlePagingClick(e);
+    });
+    return pagingNode;
+  }
+
+  async handlePagingClick(e: Event) {
+    const target = e.target as HTMLElement;
+    if (target.dataset.number) {
+      this.state.dictionary.page = +target.dataset.number;
+    } else if (target.classList.contains('paging__prev')) {
+      this.state.dictionary.page -= 1;
+    } else if (target.classList.contains('paging__next')) {
+      this.state.dictionary.page += 1;
+    }
+    await this.changeCurrentPage(this.state.dictionary.unit, this.state.dictionary.page);
+  }
+
+
+
+  units() {
+    const unitNode = <HTMLElement>unitTemplate(this.state.dictionary.unit, this.state.loggedIn).content.cloneNode(true);
+    const units = <HTMLElement>unitNode.querySelector('.units');
+    units.addEventListener('click', async (e) => {
+      this.handleUnitClick(e);
+    });
+    return unitNode;
+  }
+
+  async handleUnitClick(e: Event) {
+    console.log(this.state);
+    const target = <HTMLElement>e.target;
+    if (target.dataset.unit) {
+      this.state.dictionary.unit = +target.dataset.unit;
+    }
+    await this.changeCurrentPage(this.state.dictionary.unit, this.state.dictionary.page);
+  }
+
+  async changeCurrentPage(unit: number, page: number) {
+    window.location.hash = `/#/${this.state.page}/unit${unit}/${page}`;
+    const dictionaryProgress = { unit: this.state.dictionary.unit, page: this.state.dictionary.page };
+    const textbook = JSON.stringify(dictionaryProgress);
+    localStorage.setItem('dictionary', textbook);
+    await this.render();
+  }
+
 }
+
 
 export default Dictionary;
--- src/components/view/dictionary/DictionaryTemplate.ts
+++ src/components/view/dictionary/DictionaryTemplate.ts
@@ -1,7 +1,80 @@
-const dictionaryTemplate: HTMLTemplateElement = document.createElement('template');
-dictionaryTemplate.innerHTML = `
-  <div class="main-page">
-    <h2>Disctionary Page</h2>
-    <h3>Some Main Content</h3>
-  </div>`;
-export default dictionaryTemplate;
+import { WordData } from '../../model/types/words';
+
+// const dictionaryTemplate: HTMLTemplateElement = document.createElement('template');
+// dictionaryTemplate.innerHTML = `
+//   <div class="main-page">
+//     <h2>Disctionary Page</h2>
+//     <h3>Some Main Content</h3>
+//   </div>`;
+// export default dictionaryTemplate;
+
+
+export const sectionWords = (currentUnit: number): Record<string, HTMLElement> => {
+  const section = document.createElement('section');
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper-sec-word');
+  section.classList.add('section-word', `unit-${currentUnit}`);
+  section.append(wrapper);
+  return { section, wrapper };
+};
+
+export const titleTemplate = (titleName: string) => {
+  const titleBlock = document.createElement('template');
+  titleBlock.innerHTML = `
+  <p class="title-sec">${titleName}</p>`;
+  return titleBlock;
+};
+
+export const unitTemplate = (currentUnit: number, loggedIn: boolean): HTMLTemplateElement => {
+  const units = document.createElement('template');
+  const unitNames = [1, 2, 3, 4, 5, 6, 7];
+  const buttons = unitNames.map(
+    (unit) => `
+    <button data-unit="${unit}"
+      class="button unit-button ${unit === currentUnit ? 'current-unit' : ''}"
+      ${unit === 7 && loggedIn === false ? 'disabled="disabled"' : ''}
+      >
+      ${unit === 7 ? 'сложное' : `раздел ${unit}`}
+      ${unit === 7 ? '<div class="lable-btn"></div>' : ''}
+    </button>`,
+  )
+    .join('');
+  units.innerHTML = `
+    <div class="units">
+    ${buttons}
+    </div>`;
+  return units;
+};
+
+
+export const pagingTemplate = (currentUnit: number, currentPage: number): HTMLTemplateElement => {
+  const paging = document.createElement('template');
+  let overPages = 1;
+  const countPages = 5;
+  if ((currentPage + 2) >= 30) {
+    overPages = 26;
+  }
+  if ((currentPage + 2) < 30 && (currentPage - 2) > 1) {
+    overPages = currentPage - 2;
+  }
+  const buttons = Array.from(Array(countPages).keys())
+    .map((num) => num + overPages)
+    .map(
+      (page) => `
+        <button data-number="${page}" class="button-pag ${page === currentPage && 'current-page'}">
+          ${page}
+        </button>`,
+    )
+    .join('');
+  paging.innerHTML = `
+  <div class="wrapper-paging">
+    <div class="paging">
+      <button class="paging__prev button-pag" ${currentPage <= 1 ? 'disabled="disabled"' : ''}></button>
+      ${buttons}
+      <button class="paging__next button-pag" ${currentPage >= 30 ? 'disabled="disabled"' : ''}></button>
+    </div>
+    <a href="/#/textbook/unit${currentUnit}/${currentPage}"><button class="btn-to-menu" data-id="textbook">в учебник</button></a>
+    </div>`;
+  return paging;
+};
+
