d4818405ce5e73c9382c36996abf3947af0a9058
Mila <m.tuseeva@timeweb.ru>
2022-09-09 23:16:00 +0300

    feat: add version for mobile test

--- src/components/controller/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth-helper';
-export * from './router-helper';
-export * from './sprint-helper';
-export * from './word-helper';
--- src/components/model/types/menu-item.ts
+++
@@ -1,5 +0,0 @@
-export type MenuItem = {
-  name: string;
-  href: string;
-  auth: boolean;
-};
